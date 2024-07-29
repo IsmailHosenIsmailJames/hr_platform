ae30121a8385d0e46a27f9f94dbdf7152b4b8bed
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2024-07-29 08:33:23 +0600

    UI fix

--- lib/src/screens/home/drawer/drawer.dart
+++ lib/src/screens/home/drawer/drawer.dart
@@ -21,170 +21,172 @@ class MyDrawer extends StatelessWidget {
         (!isAdmin) ? UserModel.fromJson(box.get('userData')) : null;
 
     return Drawer(
-      child: Column(
-        children: [
-          if (!isAdmin)
-            Container(
-              height: 200,
-              width: double.infinity,
-              color: Colors.blue.shade800,
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  FittedBox(
-                    child: Text(
-                      userModel!.userName!,
+      child: SafeArea(
+        child: Column(
+          children: [
+            if (!isAdmin)
+              Container(
+                height: 200,
+                width: double.infinity,
+                color: Colors.blue.shade800,
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    FittedBox(
+                      child: Text(
+                        userModel!.userName!,
+                        style: const TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "ID: ${userModel.userID ?? ""}",
                       style: const TextStyle(
-                        fontSize: 22,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                         color: Colors.white,
                       ),
                     ),
-                  ),
-                  Text(
-                    "ID: ${userModel.userID ?? ""}",
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+                    Text(userModel.cellPhone ?? ""),
+                  ],
+                ),
+              ),
+            Expanded(
+              child: ListView(
+                padding: const EdgeInsets.all(10),
+                children: [
+                  if (FirebaseAuth.instance.currentUser!.email != null &&
+                      FirebaseAuth.instance.currentUser!.email!.isNotEmpty)
+                    TextButton.icon(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return const AddUser();
+                            },
+                          ),
+                        );
+                      },
+                      label: const Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Add user",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      icon: const Icon(FluentIcons.add_24_filled),
                     ),
+                  if (FirebaseAuth.instance.currentUser!.email != null &&
+                      FirebaseAuth.instance.currentUser!.email!.isNotEmpty)
+                    TextButton.icon(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return const Survey();
+                            },
+                          ),
+                        );
+                      },
+                      label: const Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Create a Survey",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      icon: const Icon(FluentIcons.document_table_24_regular),
+                    ),
+                  // TextButton.icon(
+                  //   onPressed: () {
+                  //     Navigator.push(
+                  //       context,
+                  //       MaterialPageRoute(
+                  //         builder: (context) {
+                  //           return const Settings();
+                  //         },
+                  //       ),
+                  //     );
+                  //   },
+                  //   label: const Row(
+                  //     mainAxisAlignment: MainAxisAlignment.start,
+                  //     children: [
+                  //       Text(
+                  //         "Settings",
+                  //         style: TextStyle(fontSize: 16),
+                  //       ),
+                  //     ],
+                  //   ),
+                  //   icon: const Icon(FluentIcons.settings_24_regular),
+                  // ),
+                  if (!(FirebaseAuth.instance.currentUser!.email != null &&
+                      FirebaseAuth.instance.currentUser!.email!.isNotEmpty))
+                    TextButton.icon(
+                      onPressed: () async {
+                        final box = await Hive.openBox('info');
+                        UserModel userModel =
+                            UserModel.fromJson(box.get('userData'));
+                        Navigator.push(
+                          // ignore: use_build_context_synchronously
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return EditProfile(
+                                userModel: userModel,
+                              );
+                            },
+                          ),
+                        );
+                      },
+                      label: const Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Edit Profile",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      icon: const Icon(FluentIcons.edit_24_regular),
+                    ),
+                  TextButton.icon(
+                    onPressed: () async {
+                      await FirebaseAuth.instance.signOut();
+                      await Hive.box('info').deleteFromDisk();
+                      await Hive.openBox("info");
+                      Navigator.pushNamedAndRemoveUntil(
+                        // ignore: use_build_context_synchronously
+                        context,
+                        '/',
+                        (route) => false,
+                      );
+                    },
+                    label: const Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Sign Out",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                    icon: const Icon(FluentIcons.sign_out_24_regular),
                   ),
-                  Text(userModel.cellPhone ?? ""),
                 ],
               ),
             ),
-          Expanded(
-            child: ListView(
-              padding: const EdgeInsets.all(10),
-              children: [
-                if (FirebaseAuth.instance.currentUser!.email != null &&
-                    FirebaseAuth.instance.currentUser!.email!.isNotEmpty)
-                  TextButton.icon(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return const AddUser();
-                          },
-                        ),
-                      );
-                    },
-                    label: const Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Add user",
-                          style: TextStyle(fontSize: 16),
-                        ),
-                      ],
-                    ),
-                    icon: const Icon(FluentIcons.add_24_filled),
-                  ),
-                if (FirebaseAuth.instance.currentUser!.email != null &&
-                    FirebaseAuth.instance.currentUser!.email!.isNotEmpty)
-                  TextButton.icon(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return const Survey();
-                          },
-                        ),
-                      );
-                    },
-                    label: const Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Create a Survey",
-                          style: TextStyle(fontSize: 16),
-                        ),
-                      ],
-                    ),
-                    icon: const Icon(FluentIcons.document_table_24_regular),
-                  ),
-                // TextButton.icon(
-                //   onPressed: () {
-                //     Navigator.push(
-                //       context,
-                //       MaterialPageRoute(
-                //         builder: (context) {
-                //           return const Settings();
-                //         },
-                //       ),
-                //     );
-                //   },
-                //   label: const Row(
-                //     mainAxisAlignment: MainAxisAlignment.start,
-                //     children: [
-                //       Text(
-                //         "Settings",
-                //         style: TextStyle(fontSize: 16),
-                //       ),
-                //     ],
-                //   ),
-                //   icon: const Icon(FluentIcons.settings_24_regular),
-                // ),
-                if (!(FirebaseAuth.instance.currentUser!.email != null &&
-                    FirebaseAuth.instance.currentUser!.email!.isNotEmpty))
-                  TextButton.icon(
-                    onPressed: () async {
-                      final box = await Hive.openBox('info');
-                      UserModel userModel =
-                          UserModel.fromJson(box.get('userData'));
-                      Navigator.push(
-                        // ignore: use_build_context_synchronously
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return EditProfile(
-                              userModel: userModel,
-                            );
-                          },
-                        ),
-                      );
-                    },
-                    label: const Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Edit Profile",
-                          style: TextStyle(fontSize: 16),
-                        ),
-                      ],
-                    ),
-                    icon: const Icon(FluentIcons.edit_24_regular),
-                  ),
-                TextButton.icon(
-                  onPressed: () async {
-                    await FirebaseAuth.instance.signOut();
-                    await Hive.box('info').deleteFromDisk();
-                    await Hive.openBox("info");
-                    Navigator.pushNamedAndRemoveUntil(
-                      // ignore: use_build_context_synchronously
-                      context,
-                      '/',
-                      (route) => false,
-                    );
-                  },
-                  label: const Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Sign Out",
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ],
-                  ),
-                  icon: const Icon(FluentIcons.sign_out_24_regular),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
