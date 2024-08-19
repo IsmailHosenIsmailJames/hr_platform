d62fe6b06cf497ea4654fe83ef046a189c2c0d15
MD. Ismail Hosen Ismail James <md.ismailhosenismailjames@gmail.com>
2024-08-19 14:26:08 +0600

    foucusNode added into all forms

--- lib/src/screens/edit_profile/edit_profile.dart
+++ lib/src/screens/edit_profile/edit_profile.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:hive_flutter/adapters.dart';
@@ -29,6 +28,17 @@ class _EditProfileState extends State<EditProfile> {
   late TextEditingController emailController;
   late TextEditingController jobTypeNameController;
   Map<String, dynamic>? extraDataTOSave;
+
+  FocusNode userNameFocusNode = FocusNode();
+  FocusNode cellPhoneFocusNode = FocusNode();
+  FocusNode companyNameFocusNode = FocusNode();
+  FocusNode dateOfJoiningFocusNode = FocusNode();
+  FocusNode departmentNameFocusNode = FocusNode();
+  FocusNode designationNameFocusNode = FocusNode();
+  FocusNode emailFocusNode = FocusNode();
+  FocusNode jobTypeNameFocusNode = FocusNode();
+  FocusNode passwordFocusNode = FocusNode();
+
   @override
   void initState() {
     passwordController =
@@ -68,6 +78,10 @@ class _EditProfileState extends State<EditProfile> {
                   titleWidget("User Password", true),
                   const Gap(5),
                   TextFormField(
+                    onFieldSubmitted: (value) {
+                      userNameFocusNode.requestFocus();
+                    },
+                    focusNode: passwordFocusNode,
                     validator: (value) {
                       if (value == null || value.length < 3) {
                         return "Password should be at least 4 digit";
@@ -84,6 +98,10 @@ class _EditProfileState extends State<EditProfile> {
                   titleWidget("User Name", false),
                   const Gap(5),
                   TextFormField(
+                    onFieldSubmitted: (value) {
+                      cellPhoneFocusNode.requestFocus();
+                    },
+                    focusNode: userNameFocusNode,
                     controller: userNameController,
                     decoration: getInputDecooration(
                       "User Name",
@@ -94,6 +112,10 @@ class _EditProfileState extends State<EditProfile> {
                   titleWidget("Cell Phone", false),
                   const Gap(5),
                   TextFormField(
+                    onFieldSubmitted: (value) {
+                      companyNameFocusNode.requestFocus();
+                    },
+                    focusNode: cellPhoneFocusNode,
                     controller: cellPhoneController,
                     decoration: getInputDecooration(
                       "Cell Phone",
@@ -104,6 +126,10 @@ class _EditProfileState extends State<EditProfile> {
                   titleWidget("Company Name", false),
                   const Gap(5),
                   TextFormField(
+                    onFieldSubmitted: (value) {
+                      dateOfJoiningFocusNode.requestFocus();
+                    },
+                    focusNode: companyNameFocusNode,
                     controller: companyNameController,
                     decoration: getInputDecooration(
                       "Company Name",
@@ -114,6 +140,10 @@ class _EditProfileState extends State<EditProfile> {
                   titleWidget("Date Of Joining", false),
                   const Gap(5),
                   TextFormField(
+                    onFieldSubmitted: (value) {
+                      departmentNameFocusNode.requestFocus();
+                    },
+                    focusNode: dateOfJoiningFocusNode,
                     controller: dateOfJoiningController,
                     decoration: getInputDecooration(
                       "Date Of Joining",
@@ -124,6 +154,10 @@ class _EditProfileState extends State<EditProfile> {
                   titleWidget("Department Name", false),
                   const Gap(5),
                   TextFormField(
+                    onFieldSubmitted: (value) {
+                      designationNameFocusNode.requestFocus();
+                    },
+                    focusNode: departmentNameFocusNode,
                     controller: departmentNameController,
                     decoration: getInputDecooration(
                       "Department Name",
@@ -134,6 +168,10 @@ class _EditProfileState extends State<EditProfile> {
                   titleWidget("Designation Name", false),
                   const Gap(5),
                   TextFormField(
+                    onFieldSubmitted: (value) {
+                      emailFocusNode.requestFocus();
+                    },
+                    focusNode: designationNameFocusNode,
                     controller: designationNameController,
                     decoration: getInputDecooration(
                       "Designation Name",
@@ -144,6 +182,10 @@ class _EditProfileState extends State<EditProfile> {
                   titleWidget("Email", false),
                   const Gap(5),
                   TextFormField(
+                    onFieldSubmitted: (value) {
+                      jobTypeNameFocusNode.requestFocus();
+                    },
+                    focusNode: emailFocusNode,
                     controller: emailController,
                     keyboardType: TextInputType.emailAddress,
                     decoration: getInputDecooration(
@@ -155,6 +197,10 @@ class _EditProfileState extends State<EditProfile> {
                   titleWidget("Job Type Name", false),
                   const Gap(5),
                   TextFormField(
+                    onFieldSubmitted: (value) async {
+                      await onSaveChangesButtonPressed(context);
+                    },
+                    focusNode: jobTypeNameFocusNode,
                     controller: jobTypeNameController,
                     decoration: getInputDecooration(
                       "Job Type",
@@ -166,50 +212,7 @@ class _EditProfileState extends State<EditProfile> {
                     width: 330,
                     child: ElevatedButton.icon(
                       onPressed: () async {
-                        if (formKey.currentState!.validate()) {
-                          try {
-                            UserModel model = UserModel(
-                              userID: widget.userModel.userID,
-                              userPassword: passwordController.text,
-                              userName: userNameController.text,
-                              cellPhone: cellPhoneController.text,
-                              companyName: companyNameController.text,
-                              dateOfJoining: dateOfJoiningController.text,
-                              departmentName: departmentNameController.text,
-                              designationName: designationNameController.text,
-                              email: emailController.text,
-                              jobTypeName: jobTypeNameController.text,
-                            );
-                            await FirebaseFirestore.instance
-                                .collection("general_user")
-                                .doc(widget.userModel.userID)
-                                .update(model.toMap());
-                            if (extraDataTOSave != null) {
-                              extraDataTOSave!.addAll(Map<String, dynamic>.from(
-                                  {widget.userModel.userID!: model.toMap()}));
-
-                              print(extraDataTOSave![widget.userModel.userID]);
-
-                              final box = await Hive.openBox("allUserCached");
-                              await box.put("data", extraDataTOSave);
-                            }
-                            Navigator.pushNamedAndRemoveUntil(
-                              // ignore: use_build_context_synchronously
-                              context,
-                              "/",
-                              (route) => false,
-                            );
-                          } catch (e) {
-                            print(e);
-                            showModalBottomSheet(
-                              // ignore: use_build_context_synchronously
-                              context: context,
-                              builder: (context) => const Center(
-                                child: Text("Somethings went worng"),
-                              ),
-                            );
-                          }
-                        }
+                        await onSaveChangesButtonPressed(context);
                       },
                       label: const Text("Save Changes"),
                       icon: const Icon(Icons.done),
@@ -222,5 +225,49 @@ class _EditProfileState extends State<EditProfile> {
         ),
       ),
     );
+  }
+
+  Future<void> onSaveChangesButtonPressed(BuildContext context) async {
+    if (formKey.currentState!.validate()) {
+      try {
+        UserModel model = UserModel(
+          userID: widget.userModel.userID,
+          userPassword: passwordController.text,
+          userName: userNameController.text,
+          cellPhone: cellPhoneController.text,
+          companyName: companyNameController.text,
+          dateOfJoining: dateOfJoiningController.text,
+          departmentName: departmentNameController.text,
+          designationName: designationNameController.text,
+          email: emailController.text,
+          jobTypeName: jobTypeNameController.text,
+        );
+        await FirebaseFirestore.instance
+            .collection("general_user")
+            .doc(widget.userModel.userID)
+            .update(model.toMap());
+        if (extraDataTOSave != null) {
+          extraDataTOSave!.addAll(Map<String, dynamic>.from(
+              {widget.userModel.userID!: model.toMap()}));
+
+          final box = await Hive.openBox("allUserCached");
+          await box.put("data", extraDataTOSave);
+        }
+        Navigator.pushNamedAndRemoveUntil(
+          // ignore: use_build_context_synchronously
+          context,
+          "/",
+          (route) => false,
+        );
+      } catch (e) {
+        showModalBottomSheet(
+          // ignore: use_build_context_synchronously
+          context: context,
+          builder: (context) => const Center(
+            child: Text("Somethings went worng"),
+          ),
+        );
+      }
+    }
   }
 }
